e12cb9f6cbd7b05f093b9bdc272327598a523ba0
Gheruha <maxgheruha@gmail.com>
2025-05-21 10:23:51 +0300

    Made utility functions for signing up

---
+++ store/user.mapper.ts
@@ -0,0 +1,13 @@
+import { User } from "@/types/user.type";
+export interface UserResponse {
+  id: string;
+  email?: string;
+}
+export const mapUserData = async (
+  responseData: UserResponse
+): Promise<User> => {
+  return {
+    id: responseData.id,
+    email: responseData.email,
+  };
+};
---
+++ utils/auth/auth.utils.ts
@@ -0,0 +1,44 @@
+import { SignUpDto } from "@/types/auth.type";
+import { Session } from "@supabase/supabase-js";
+import { createSupabaseClientApi } from "../../supabase/client";
+import { mapUserData } from "../../store/user.mapper";
+
+// Ta
+export const authWithCode = async (code: string): Promise<void> => {
+  const supabase = await createSupabaseClientApi();
+  const { error } = await supabase.auth.exchangeCodeForSession(code);
+
+  if (error) {
+    console.error("Error exchanging code for session:", error.message);
+    throw new Error(error.message || "Failed to exchange code for session");
+  }
+};
+
+// signUpUser receives data for auth and is doing a session for the user
+export const signUpUser = async ({
+  email,
+  password,
+  redirectUrl,
+}: SignUpDto): Promise<Session | null> => {
+  const supabase = await createSupabaseClientApi();
+
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: redirectUrl,
+    },
+  });
+
+  if (error) {
+    console.error("Error signin up", error.message);
+    throw new Error(error.message || "Failed to sign up");
+  }
+
+  if (!data.user) {
+    throw new Error("User data is missing after sign-up");
+  }
+  await mapUserData(data.user);
+
+  return data.session;
+};
